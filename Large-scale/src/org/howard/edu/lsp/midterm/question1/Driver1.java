6ce219ee5d7ac7920ed178cb8f9b209f2b392217
DS Mentor <sunishchit.ghimire@bison.howard.edu>
2024-03-14 20:02:57 -0400

    This is my Question 1

---
+++ Large-scale/src/org/howard/edu/lsp/midterm/question1/Driver1.java
@@ -0,0 +1,19 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class Driver1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SecurityOOPS obj = new SecurityOOPS();
+		String testOutput = obj.encrypt("I love CSCI363");
+		System.out.println(testOutput);
+//	        String originalText = "I love CSCI363";
+//	        String encryptedText = SecurityOOPS.encrypt(originalText);
+//	        
+//	        System.out.println("Original Text: " + originalText);
+//	        System.out.println("Encrypted Text: " + encryptedText);
+	   
+
+	}
+
+}
---
+++ Large-scale/src/org/howard/edu/lsp/midterm/question1/SecurityOOPS.java
@@ -0,0 +1,47 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class SecurityOOPS {
+	/**
+     * Encrypts a given text by rearranging characters based on their index.
+     * 
+     * @param text The text to be encrypted
+     * @return The encrypted text
+     */
+    public static String encrypt(String text) {
+        String evenIndexChars = "";
+        String oddIndexChars = "";
+        String temp = "";
+        
+        // Loop through characters
+        
+        for(int i=0;i<text.length(); i++) {
+        	if(Character.isWhitespace(text.charAt(i))) {
+        		continue;
+        	}
+        	temp+= text.charAt(i);
+        }
+        for (int i = 0; i < temp.length(); i++) {
+            char currentChar = temp.charAt(i);
+            // Ignore punctuation and whitespace
+            if (Character.isLetterOrDigit(currentChar)) {
+                // Append even-indexed characters
+                if (i % 2 == 0) {
+                    evenIndexChars+= currentChar;
+                    continue;
+                } 
+                // Append odd-indexed characters
+                else {
+                    oddIndexChars+=currentChar;
+                    continue;
+                }
+            }
+        }
+        
+        // Combine even-indexed and odd-indexed characters
+        String finalOutput = evenIndexChars + oddIndexChars;
+        return finalOutput;
+    }
+	
+	
+
+}
